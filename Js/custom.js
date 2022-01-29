03485fd9a30c380a97b9676975ca995c7374817a
YordanisPonce <82534079+YordanisPonce@users.noreply.github.com>
2022-01-29 15:47:36 +0100

    Update custom.js

--- Js/custom.js
+++ Js/custom.js
@@ -1,18 +1,1 @@
-function responsiveDesign() {
-    let container = $("#nav_bar");
-    $(window).resize(() => {
-        if ($(this).width() <= 400) {
-            container.removeClass("container");
-        } else {
-            container.addClass("container");
-        }
-/* 
-        if ($(this).width() <= 1075) {
-            container.removeClass("justify-content-lg-bettween");
-            container.addClass("justify-content-center");
-        } else {
-            container.addClass("justify-content-lg-bettween");
-            container.removeClass("justify-content-center");
-        } */
-    });
-}
+
