958e7efd803d955aacf77d26b4c6b5fa627a3431
unknown <yordisponcealderete@gmail.com>
2022-02-12 22:01:00 +0100

    Commit modal packages successfully

--- Js/custom.js
+++ Js/custom.js
@@ -9,3 +9,43 @@ function responsiveDesign() {
         }
     });
 }
+function getPackage(packageType) {
+    let type = ``;
+    if (packageType == 1) {
+        type = "Básico";
+    } else if (packageType == 2) {
+        type = "Pro";
+    } else if (packageType == 3) {
+        type = "Golden";
+    }
+    $(`#package-name`).text(type);
+}
+function replaceInputGroup(cont) {
+    if (cont == 0) {
+        $(`.input-group-${cont}`).addClass("d-none");
+        $(`.input-group-${cont + 1}`).removeClass("d-none");
+    } else if (cont == 1) {
+        $(`.input-group-${cont}`).addClass("d-none");
+        $(`.input-group-${cont + 1}`).removeClass("d-none");
+        $(`#continue`).text("Aceptar");
+    } else if (cont == 2) {
+        $(`.input-group-${cont}`).addClass("d-none");
+        $(`.input-group-${0}`).removeClass("d-none")
+        $("#form-modal").modal(`hide`);
+    }
+
+}
+function resetModal() {
+    $("#form_packages").trigger("reset"); 
+  /*   cont = 0;
+    for (let i = 0; i <= 2; i++) {
+        if (i == 0) {
+            $(`.input-group-${i}`).show()
+        } else {
+            $(`.input-group-${i}`).hide();
+        }
+
+
+    }
+ */
+}  
