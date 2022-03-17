fd7cca833ccd9c6b3475ed78d0330cb96608ff8c
unknown <yordisponcealderete@gmail.com>
2022-03-17 22:24:11 +0100

    New changes commit from adrian

--- Js/packages.js
+++ Js/packages.js
@@ -3,6 +3,7 @@ function toggleNavBar() {
     if ($(window).height() <= 530 || $(window).width() <= 575) {
         container.addClass("sticky-top");
         container.removeClass("fixed-top");
+        
     }
     $(window).resize(() => {
         if ($(window).height() <= 530 || $(window).width() <= 575) {
